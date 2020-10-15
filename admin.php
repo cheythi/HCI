3398a58a87a521e31779d108b5a6584058ec317b
cheythi <chethanageeth@gmail.com>
2020-10-15 13:51:06 +0530

    new header for logged users

--- admin.php
+++ admin.php
@@ -1,4 +1,4 @@
-<?php include 'header.php'; ?><!--need to add new header with logout btn-->
+<?php include 'headerLogged.php'; ?><!--need to add new header with logout btn-->
 
 <section id="adminloginpage" class="container-fluid">
 <div class="container">
